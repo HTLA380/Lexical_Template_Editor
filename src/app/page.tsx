47c4bd4836b80643c57a4bde0c8e6b557e194e0f
Htet Aung Lin <htetlin099798@gmail.com>
2024-03-24 11:29:39 +0630

    feat: edit index page layout and elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,27 @@
 import React from "react";
-import Tiptap from "../components/editor/Tiptap";
 import Link from "next/link";
-// import TiptapToolbar from "../components/editor/TiptapToolbar";
 
 const Home = () => {
   return (
-    <main className="bg-slate-100 px-5 pb-10 pt-20">
-      <Tiptap />
-      <Link
-        href="/output"
-        className="fixed bottom-11 right-12 rounded-md bg-blue-500 px-3 py-1.5 text-white"
-      >
-        Output
-      </Link>
-    </main>
+    <div className="min-h-screen bg-slate-100 p-5">
+      <nav className="mx-auto flex w-full max-w-6xl items-center justify-between border-b border-gray-900 pb-5">
+        <h1>Lexical Template</h1>
+        <div className="flex items-center gap-2 ">
+          <button className="rounded-md bg-gray-900 px-3 py-2 text-sm text-white shadow-sm transition-colors hover:bg-gray-700">
+            Template List
+          </button>
+          <Link
+            href={"/create-template"}
+            className="rounded-md border border-gray-800 bg-transparent px-3 py-2 text-sm text-gray-900 shadow-sm hover:bg-gray-300"
+          >
+            Create New Template
+          </Link>
+        </div>
+      </nav>
+      <main className="mx-auto mt-5 flex min-h-screen w-full max-w-6xl justify-center">
+        Currently, no template was made yet. Please create one first.
+      </main>
+    </div>
   );
 };
 
