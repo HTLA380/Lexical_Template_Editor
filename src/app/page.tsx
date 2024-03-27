40ebe047146cd2bd1e1566399a734cee6da24477
Htet Aung Lin <htetlin099798@gmail.com>
2024-03-27 16:02:32 +0630

    refactor: refactor index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,7 @@ import Link from "next/link";
 import { useLocalStorageContext } from "@/context/LocalStorageContext";
 
 const Home = () => {
-  const { templates } = useLocalStorageContext();
-
-  const placeHolder = (
-    <p>Currently, no template was made yet. Please create one first.</p>
-  );
+  const { templates: templateList } = useLocalStorageContext();
 
   return (
     <div className="min-h-screen bg-slate-100 p-5">
@@ -20,39 +16,35 @@ const Home = () => {
             Template List
           </button>
           <Link
-            href={"template/create"}
+            href="/template/create"
             className="rounded-md border border-gray-800 bg-transparent px-3 py-2 text-sm text-gray-900 shadow-sm hover:bg-gray-300">
             Create New Template
           </Link>
         </div>
       </nav>
-      <main className="mx-auto mt-5  flex min-h-screen w-full max-w-6xl flex-col items-center gap-5">
-        {templates
-          ? templates.map((template) => (
-              <div
-                key={template.id}
-                className="flex w-full items-center justify-between rounded-md border border-gray-800 px-3 py-3">
-                <div className="text-sm">
-                  <p>{template.title}</p>
-                  <p className="text-gray-600">
-                    Created At {template.createAt}
-                  </p>
-                </div>
-                <div className="flex items-center justify-center gap-2">
-                  <Link
-                    className="rounded-md border border-gray-800 bg-gray-800 px-4 py-1.5 text-sm text-white transition-colors hover:bg-transparent hover:text-black"
-                    href={`/template/edit/${template.id}`}>
-                    Edit
-                  </Link>
-                  <Link
-                    className="rounded-md border border-gray-800 bg-transparent px-4 py-1.5 text-sm text-black transition-colors hover:bg-gray-800 hover:text-white"
-                    href={`/template/edit/${template.id}`}>
-                    Fill Template
-                  </Link>
-                </div>
-              </div>
-            ))
-          : placeHolder}
+      <main className="mx-auto mt-5 flex min-h-screen w-full max-w-6xl flex-col items-center gap-5">
+        {templateList?.map((template) => (
+          <div
+            key={template.id}
+            className="flex w-full items-center justify-between rounded-md border border-gray-800 px-3 py-3">
+            <div className="text-sm">
+              <p>{template.title}</p>
+              <p className="text-gray-600">Created At {template.createAt}</p>
+            </div>
+            <div className="flex items-center justify-center gap-2">
+              <Link
+                href={`/template/edit/${template.id}`}
+                className="rounded-md border border-gray-800 bg-gray-800 px-4 py-1.5 text-sm text-white transition-colors hover:bg-transparent hover:text-black">
+                Edit
+              </Link>
+              <Link
+                href={`/template/fill/${template.id}`}
+                className="rounded-md border border-gray-800 bg-transparent px-4 py-1.5 text-sm text-black transition-colors hover:bg-gray-800 hover:text-white">
+                Fill Template
+              </Link>
+            </div>
+          </div>
+        ))}
       </main>
     </div>
   );
